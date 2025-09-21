7ee049e2379cbc79fee72a16da6329772452e59c
mitate-gengaku <mitategengaku@gmail.com>
2025-09-21 22:35:33 +0900

    add: compress

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,11 +1,22 @@
 // @ts-check
+import path from "path";
+
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import relativeLinks from "astro-relative-links";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    relativeLinks(),
+    (await import("astro-compress")).default({
+      HTML: {
+        "html-minifier-terser": false,
+      },
+    }),
+  ],
 
   server: {
     port: 3000,
@@ -14,5 +25,12 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(new URL(".", import.meta.url).pathname, "src"),
+      },
+    },
   },
+
+  compressHTML: false,
 });
